2fa8c8676ca621070c71d14de2574e1006e48407
Harsh2004R <harshsharmaktm03@gmail.com>
2025-09-07 14:35:05 +0530

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,27 @@ app.get("/", (req, res) => {
 // Import routes
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Error handling middleware
 app.use(errorHandler);
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
