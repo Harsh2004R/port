942f6013794822b700b086d3efeea6560c67052d
Harsh2004R <harshsharmaktm03@gmail.com>
2025-09-08 22:32:15 +0530

    Update Route

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createContact, getContacts } = require('../controllers/contactController');
+const { createContact, getContacts, deleteContact, replyToContact } = require('../controllers/contactController');
 const adminAuth = require('../middleware/adminAuth');
 
 // Public: create a contact submission
@@ -8,6 +8,12 @@ router.post('/', createContact);
 
 // Admin: list contact submissions
 router.get('/', adminAuth, getContacts);
+
+// Admin: delete a contact submission
+router.delete('/:id', adminAuth, deleteContact);
+
+// Admin: reply to a contact submission
+router.post('/:id/reply', adminAuth, replyToContact);
 
 module.exports = router;
 
